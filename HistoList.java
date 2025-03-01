e69e77beb996611383cb758880d253a71d4085d4
geode3 <132504158+geode3@users.noreply.github.com>
2025-02-28 22:32:48 -0600

    Update HistoList.java

--- HistoList.java
+++ HistoList.java
@@ -1,80 +1,97 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
+//Name - Giovani Zuniga
 
 import java.util.*;
 import static java.lang.System.*;
 
-public class HistoList
-{
-   private HistoNode front;
+public class HistoList {
+    private HistoNode front;
+    /**
+     * Constructs an empty HistoList
+     */
+    public HistoList() {
+        front = null;
+    }
 
-	public HistoList( )
-	{
-		front = null;
-	}
+    /**
+     * Adds a letter to the histogram. If the letter already exists,
+     * its count is incremented otherwise a new node is added at the front
+     * @param let the character to be added
+     */
+    public void addLetter(char let) {
+        if (front == null) {
+            front = new HistoNode(let, 1, null);
+            return;
+        }
 
-	//addLetter will add a new node to the front for let if let does not exist
-	//addLetter will bump up the count if let already exists
-	public void addLetter(char let)
-	{
+        HistoNode current = front;
+        HistoNode prev = null;
+        
+        while (current != null) {
+            if (current.getLetter() == let) {
+                current.setLetterCount(current.getLetterCount() + 1);
+                return;
+            }
+            prev = current;
+            current = current.getNext();
+        }
+        
+        // Letter not found, add new node to the front
+        front = new HistoNode(let, 1, front);
+    }
 
+   /**
+     * Returns the index position of a given letter in the list
+     * @param let the character to search for
+     * @return the index position of the character or -1 if not found
+     */
+    public int indexOf(char let) {
+        HistoNode current = front;
+        int index = 0;
+        
+        while (current != null) {
+            if (current.getLetter() == let) {
+                return index;
+            }
+            current = current.getNext();
+            index++;
+        }
+        return -1;
+    }
 
+    /**
+     * Returns a reference to the node at the given position
+     * @param spot the index position of the node
+     * @return the HistoNode at the given index or null if out of bounds
+     */
+    private HistoNode nodeAt(int spot) {
+        HistoNode current = front;
+        int index = 0;
+        
+        while (current != null) {
+            if (index == spot) {
+                return current;
+            }
+            current = current.getNext();
+            index++;
+        }
+        return null;
+    }
 
-
-
-
-
-
-	}
-
-	//returns the index pos of let in the list if let exists
-	public int indexOf(char let)
-	{
-
-
-
-
-
-
-
-
-
-		return -1;
-	}
-
-	//returns a reference to the node at spot
-	private HistoNode nodeAt(int spot)
-	{
-		HistoNode current=null;
-
-
-
-
-
-
-
-
-
-
-		return current;
-	}
-
-	//returns a string will all values from list
-	public String toString()
-	{
-		String output = "";
-
-
-
-
-
-
-
-
-
-
-		return output;
-	}
+   /**
+     * Returns a string representation of the histogram list
+     * @return a formatted string of characters and their counts
+     */
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+        HistoNode current = front;
+        
+        while (current != null) {
+            output.append(current.getLetter()).append(" - ").append(current.getLetterCount()).append("     ");
+            current = current.getNext();
+        }
+        return output.toString();
+    }
 }
